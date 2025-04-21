771832d30f577534771e97796378512d720f1a6f
Tiago <tiagohslino@gmail.com>
2025-04-21 12:27:26 -0300

    add App.js and css

---
+++ src/App.js
@@ -0,0 +1,21 @@
+export default function App() {
+    return (
+        <div className="flashcards">
+            <div className="card">
+                Cartão 1
+            </div>
+            <div className="card">
+                Cartão 2
+            </div>
+            <div className="card">
+                Cartão 3
+            </div>
+            <div className="card">
+                Cartão 4
+            </div>
+            <div className="card">
+                Cartão 5
+            </div>
+        </div>
+    );
+}
